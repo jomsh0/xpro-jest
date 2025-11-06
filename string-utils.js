67b5085a362136ccce38b70dcac4da2891d7d58b
John Sherrell <dev@ohn.sh>
2025-11-06 02:54:05 -0600

    Fix up comments.

--- string-utils.js
+++ string-utils.js
@@ -12,17 +12,20 @@ export function capitalizeWords(str) {
   return str.split(" ").map(word => word.slice(0, 1).toUpperCase() + word.slice(1)).join(" ")
 }
 
-// The first letter of the first word should always be capitalized.
-// The list of words to ignore can be found in the 'nonCapitalizedWords' array.
 export function toTitleCase(str) {
+  // Utility to capitalize a single word
   const capitalize = word => word.slice(0, 1).toUpperCase() + word.slice(1)
 
+  // The list of words to ignore can be found in the 'nonCapitalizedWords' array.
   const capWords = str.split(" ").map(
     word => nonCapitalizedWords.includes(word)
       ? word
       : capitalize(word)
   )
+
+  // The first letter of the first word should always be capitalized.
   capWords[0] = capWords[0] && capitalize(capWords[0])
+
   return capWords.join(" ")
 }
 
